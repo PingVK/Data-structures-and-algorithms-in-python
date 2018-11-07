65aa5122b1fdb3a7ad266ccb0787d92e3f132b40
PingVK <39548562+PingVK@users.noreply.github.com>
2018-11-07 16:25:30 +0800

    Cyclic single linked list

--- LinkedList.py
+++ LinkedList.py
@@ -117,16 +117,65 @@ class LList1(LList):
         return e
 
 
+class LCList:
+    """Cyclic single linked list"""
+    def __init__(self):
+        self._rear = None
+
+    def is_empty(self):
+        return self._rear is None
+
+    def prepend(self, elem):
+        p = LNode(elem)
+        if self._rear is None:
+            p.next = p
+            self._rear = p
+        else:
+            p.next = self._rear.next
+            self._rear.next = p
+
+    def append(self, elem):
+        self.prepend(elem)
+        self._rear = self._rear.next
+
+    def pop(self):
+        if self._rear is None:
+            raise LinkedListUnderflow("in pop of CLList")
+        p = self._rear.next
+        if self._rear is p:
+            self._rear = None
+        else:
+            self._rear.next = p.next
+        return p.elem
+
+    def printall(self):
+        if self.is_empty():
+            return
+        p = self._rear.next
+        while True:
+            if p is self._rear:
+                print(p.elem)
+                break
+            else:
+                print(p.elem, end=', ')
+            p = p.next
+
+
 if __name__ == "__main__":
     mlist = LList()
     for i in range(10):
         mlist.append(i)
     mlist.printall()
 
-    mlist1 = LList1()
-    mlist1.prepend(99)
-    for i in range(11, 20):
-        mlist1.append(i)
-    for x in mlist1.filter(lambda y: y % 2 == 0):
-        print(x, end=' ')
-    print()
+    # mlist1 = LList1()
+    # mlist1.prepend(99)
+    # for i in range(11, 20):
+    #     mlist1.append(i)
+    # for x in mlist1.filter(lambda y: y % 2 == 0):
+    #     print(x, end=' ')
+    # print()
+
+    # lclist = LCList()
+    # for i in range(10):
+    #     lclist.append(i)
+    # lclist.printall()
