4e15cc941c555f9bd4b368c06321f0447bd96c21
PingVK <39548562+PingVK@users.noreply.github.com>
2018-11-08 15:07:31 +0800

    double linked list

--- LinkedList.py
+++ LinkedList.py
@@ -161,6 +161,56 @@ class LCList:
             p = p.next
 
 
+class DLNode(LNode):
+    """Double Linked Node"""
+    def __init__(self, elem, prev=None, next_=None):
+        super(DLNode, self).__init__(elem, next_)
+        self.prev = prev
+
+
+class DLList(LList1):
+    def __init__(self):
+        super(DLList, self).__init__()
+
+    def prepend(self, elem):
+        p = DLNode(elem, None, self._head)
+        if self._head is None:
+            self._rear = p
+        else:
+            p.next.prev = p
+        self._head = p
+
+    def append(self, elem):
+        p = DLNode(elem, self._rear, None)
+        if self._head is None:
+            self._head = p
+        else:
+            p.prev.next = p
+        self._rear = p
+
+    def pop(self):
+        if self._head is None:
+            raise LinkedListUnderflow("in pop of DLList")
+        e = self._head.elem
+        self._head = self._head.next
+        if self._head is not None:
+            self._head.prev = None
+        # else:
+        #     self._rear = None
+        return e
+
+    def pop_last(self):
+        if self._head is None:
+            raise LinkedListUnderflow("in pop_last of DLList")
+        e = self._rear.elem
+        self._rear = self._rear.prev
+        if self._rear is None:
+            self._head = None
+        else:
+            self._rear.next = None
+        return e
+
+
 if __name__ == "__main__":
     mlist = LList()
     for i in range(10):
