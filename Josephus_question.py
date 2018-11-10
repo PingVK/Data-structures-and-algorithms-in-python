67392a4708f4b5c35c0abfa775ff14b74787cb2c
PingVK <39548562+PingVK@users.noreply.github.com>
2018-11-10 19:32:34 +0800

    josephus problem

---
+++ Josephus_question.py
@@ -0,0 +1,22 @@
+"""
+假设有n个人围坐一圈，现在要求从第k个人开始报数，报到第m个数的人退出。然后从下一个人开始继续报数并按相同规则退出，
+直至所有人退出。
+要求按顺序输出各出列人的编号。
+"""
+
+from LinkedList import LCList
+
+
+def josephus_a(n, k, m):
+    """Based on array"""
+    pass
+
+
+def josephus_l(n, k, m):
+    """Based on list"""
+    pass
+
+
+class Josephus(LCList):
+    """Based on cyclic single linked list"""
+    pass
